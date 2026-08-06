f8423a7227fe60af86149d88fad37107795e805f
nikam1212 <github5258@gmail.com>
2026-08-06 14:09:07 +0530

    Add files via upload

---
+++ String Formats.py
@@ -0,0 +1,24 @@
+name = input("Enter your Name :")
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.capitalize())
+print(name.swapcase())
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+print(name.replace("yash","karan"))
+print(name.split())
+print(name.find("s"))
+print(name.count("h"))
+print(name.center(50)) 
+print(name.ljust(50))
+print(name.rjust(50))
+
+
+
+
+
+
+
+
