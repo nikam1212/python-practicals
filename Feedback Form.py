0bf7351b1f98a10fc6398b1fae43e927a18073c3
nikam1212 <github5258@gmail.com>
2026-08-13 12:36:47 +0530

    Add files via upload

---
+++ Feedback Form.py
@@ -0,0 +1,14 @@
+customer_name=input("Enter Customer Name :")
+product_name=input("Enter Product Name :")
+feedback=input("Enter your Feedback :")
+
+customer_name=customer_name.strip().title()
+product_name=product_name.strip().title()
+feedback=feedback.capitalize().strip()
+
+print("==========feedback===========")
+print("Customer Name :",customer_name)
+print("Product Name :",product_name)
+print("Feedback :",feedback)
+
+print("======thankyou for your feedback==========")
